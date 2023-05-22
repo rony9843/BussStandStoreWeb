188d2591c1be1535a9b9d9c464b3bedb2a4a3c14
Jubayth Hossen Roni <84420078+rony9843@users.noreply.github.com>
2023-05-22 10:37:08 +0300

    add barcode reder and state

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -2,9 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 
 export default function AddProduct() {
   const [inputProductQrCode, setInputProductQrCode] = useState("");
-  const [productQrCode, setProductQrCode] = useState("");
+  const [productQrCode, setProductQrCode] = useState([]);
+  const [productQrCodeWithQty, setProductQrCodeWithQty] = useState([]);
 
-  const [cursor, setCursor] = useState(null);
   const ref = useRef(null);
 
   useEffect(() => {
@@ -16,12 +16,38 @@ export default function AddProduct() {
     }
   }, [inputProductQrCode]);
 
+  useEffect(() => {
+    const data = productQrCode;
+
+    const result = data.reduce(function (prev, curr) {
+      const index = prev.findIndex((el) => el.name === curr);
+
+      if (index !== -1) {
+        prev[index].count += 1;
+      } else {
+        prev.push({ name: curr, count: 1 });
+      }
+
+      return prev;
+    }, []);
+
+    console.log(result);
+    setProductQrCodeWithQty(result);
+  }, [productQrCode]);
+
   const timerQrCode = () => {
     setTimeout(() => {
-      setProductQrCode(inputProductQrCode);
+      setProductQrCode([...productQrCode, inputProductQrCode]);
+
       setInputProductQrCode("");
       ref.current.focus();
+
+      filterArray();
     }, 500);
+  };
+
+  const filterArray = () => {
+    console.log("tt");
   };
 
   return (
@@ -35,8 +61,16 @@ export default function AddProduct() {
         />
       </div>
       <div>
-        <h2>Value : {productQrCode}</h2>
+        <h2>
+          Value :{" "}
+          {productQrCodeWithQty.map((dt) => (
+            <h3>
+              barcode : {dt.name} - Qty : {dt.count}
+            </h3>
+          ))}
+        </h2>
       </div>
+      <button onClick={() => ref.current.focus()}>focus</button>
     </div>
   );
 }
